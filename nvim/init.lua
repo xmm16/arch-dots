d6b0202a80f82fabea74a0d4095470ee7b7a50cc
Earl Dish <eshaan2031@icloud.com>
2025-12-05 20:57:26 -0600

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,22 @@
--- Basic options
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.mouse = "a"
-vim.opt.termguicolors = true
-
 vim.api.nvim_set_keymap('n', ',', '<C-w>', { noremap = true })
 vim.keymap.set({'n','v','i'}, '<PageUp>', '<Nop>')
 vim.keymap.set({'n','v','i'}, '<PageDown>', '<Nop>')
 
--- Highlight tweaks
+vim.opt.termguicolors = true
 vim.cmd [[
   highlight Normal guibg=#1e1e1e guifg=#cdd6f4
   highlight NormalNC ctermbg=NONE
   highlight SignColumn ctermbg=NONE
   highlight LineNr ctermbg=NONE
   highlight EndOfBuffer ctermbg=NONE
-  highlight Pmenu ctermbg=NONE ctermfg=NONE
-  highlight PmenuSel ctermbg=NONE ctermfg=NONE
-  highlight PmenuThumb ctermbg=NONE
-  highlight FloatBorder ctermbg=NONE ctermfg=NONE
-  highlight NormalFloat ctermbg=NONE ctermfg=NONE
 ]]
 
--- Lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data").."/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -49,61 +40,46 @@ require("lazy").setup({
       ]]
     end
   },
-  { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "L3MON4D3/LuaSnip" },
-  { "saadparwaiz1/cmp_luasnip" },
-  { "nvim-lua/plenary.nvim" },
+  { "neovim/nvim-lspconfig", lazy = true },
+  { "williamboman/mason.nvim", lazy = true },
+  { "williamboman/mason-lspconfig.nvim", lazy = true },
+  { "hrsh7th/nvim-cmp", lazy = true },
+  { "hrsh7th/cmp-nvim-lsp", lazy = true },
+  { "L3MON4D3/LuaSnip", lazy = true },
+  { "saadparwaiz1/cmp_luasnip", lazy = true },
+  { "nvim-lua/plenary.nvim", lazy = true },
   {
     "nvim-treesitter/nvim-treesitter",
-    build = function() vim.cmd("TSUpdateSync") end,
+    build = ":TSUpdateSync",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "cpp" },
         highlight = { enable = true },
         incremental_selection = { enable = false },
         indent = { enable = false },
-        disable = function(_, bufnr)
-          return vim.api.nvim_buf_line_count(bufnr) > 500
-        end
       })
-    end
+    end,
+    lazy = true,
   },
   {
     "nvimtools/none-ls.nvim",
+    lazy = true,
     config = function()
       local null_ls = require("null-ls")
       null_ls.setup({
         sources = { null_ls.builtins.formatting.clang_format },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ async = true })
-              end
-            })
-          end
-        end,
       })
     end
   },
 })
 
--- Comment & autopairs
 require("Comment").setup()
 require("nvim-autopairs").setup({
-  check_ts = true,
-  enable_check_bracket_line = false,
+  check_ts = false,  -- faster
   map_cr = true,
   enable_moveright = true,
   disable_filetype = { "TelescopePrompt" }
 })
 
--- CMP setup (manual trigger only)
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 cmp.setup({
@@ -112,31 +88,46 @@ cmp.setup({
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
   }),
-  completion = { autocomplete = false },
+  completion = { autocomplete = false }, -- manual completion
   sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "luasnip" } }),
 })
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
--- Mason & LSP setup
-require("mason").setup()
-require("mason-lspconfig").setup({ ensure_installed = { "clangd" } })
-
-local lspconfig = require("lspconfig")
-lspconfig.clangd.setup({
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
-  init_options = { semanticHighlighting = false },
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp,c,h,hpp",
+  callback = function()
+    local lspconfig = require("lspconfig")
+    lspconfig.clangd.setup({
+      cmd = { "clangd", "--background-index", "--clang-tidy", "--completion-style=detailed", "--limit-results=500" },
+      flags = { debounce_text_changes = 500 },
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      on_attach = function(client, bufnr)
+        client.server_capabilities.semanticTokensProvider = nil  -- disable semantic tokens
+        local opts = { buffer = bufnr, silent = true }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      end,
+    })
+  end
 })
 
--- Diagnostics
 vim.diagnostic.config({
   float = { border = "single" },
-  virtual_text = true,
+  virtual_text = false, -- avoid lag
   signs = true,
   update_in_insert = false,
 })
 
--- Netrw
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
+
+vim.cmd [[
+  highlight Pmenu ctermbg=NONE ctermfg=NONE
+  highlight PmenuSel ctermbg=NONE ctermfg=NONE
+  highlight PmenuThumb ctermbg=NONE
+  highlight FloatBorder ctermbg=NONE ctermfg=NONE
+  highlight NormalFloat ctermbg=NONE ctermfg=NONE
+]]
