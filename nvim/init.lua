daf9b32a7db9a8f57332f7a5502e912c25f5ca1a
Earl Dish <eshaan2031@icloud.com>
2025-12-05 20:36:02 -0600

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -88,8 +88,6 @@ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 require("mason").setup()
-require("mason-lspconfig").setup({ ensure_installed = { "clangd" } })
-
 vim.lsp.config("clangd", {
   cmd = { "clangd", "--tweaks=-std=c++23" },
 })
